1106cf82e5069487c3fa4f79d0e16d87d2524aa8
B14-10 <B14-10@cu.local>
2021-10-15 22:08:46 +0400

    commi1

--- src/test/java/task1.java
+++ src/test/java/task1.java
@@ -3,6 +3,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
 import java.util.List;
@@ -14,35 +15,14 @@ public class task1
     {
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
-        driver.get("http://the-internet.herokuapp.com/add_remove_elements/");
+        driver.get("https://demoqa.com/progress-bar");
         driver.manage().window().maximize();
 
-        for(int i=1;i<=3;i++)
-        {
+            driver.findElement(By.id("startStopButton")).click();
+            String  innerText = driver.findElement(By.id("startStopButton")).getText();
+            System.out.println(innerText);
 
-            driver.findElement(By.xpath("/html/body/div[2]/div/div/button")).click();
-
-        }
-        String last_1 = driver.findElement(By.xpath("/html/body/div[2]/div/div/div/button[3]")).getText();
-        System.out.println(last_1);
-
-        List<WebElement> elements = driver.findElements(By.cssSelector("button[class^='added']"));
-        int zoma = elements.size();
-        System.out.println(elements.get(zoma-1).getText());
-
-        LoremIpsum();
     }
 
-    public  void  LoremIpsum()
-    {
-        WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
 
-        driver.get("http://the-internet.herokuapp.com/challenging_dom");
-        WebElement selectCnt = driver.findElement(By.xpath("//*[@id='content']/div/div/div/div[2]/table//*[contains(text(), 'Apeirian9')]"));
-        System.out.println(selectCnt.getText());
-
-        WebElement followingSibling = selectCnt.findElement(By.xpath("following-sibling::*"));
-        System.out.println(followingSibling.getText());
-    }
 }
